ebabfbad6b5cf03926abaaffab4e2a8769c11abe
Mahek0105 <maheksharma501@gmail.com>
2025-10-15 09:21:53 +0530

    first commit

---
+++ git/src/App.js
@@ -0,0 +1,28 @@
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum);
+  }
+  return (
+    <>
+    <form onSubmit = {handleSubmit}>
+      <input type ="number" placeholder="Enter First Number" value={num1} onChange={(e) => setNum1(e.target.value)}></input>
+      <input type ="number" placeholder="Enter Second Number" value={num2} onChange={(e) => setNum2(e.target.value)} ></input>
+
+      <button type='submit'>Add</button>
+      <h2>Result : {result}</h2>
+      </form></>
+  );
+}
+
+export default App;
